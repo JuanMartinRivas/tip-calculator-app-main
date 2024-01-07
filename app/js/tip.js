936b6f644346e8414ae674001a7c27a0b923bacf
JuanMartinRivas <jmartinrivas14@gmail.com>
2024-01-07 13:42:00 -0300

    Writing error, reset and empty function
    Writing a handleError function that displays an error message when the people number input is empty, a reset function that clears the entire card and an empty function that adds an empty class to the reset button whenever the inputs are empty

--- app/js/tip.js
+++ app/js/tip.js
@@ -7,6 +7,8 @@ const tipAmountText = document.getElementById('tip-amount');
 const totalAmountText = document.getElementById('total-amount');
 const resetBtn = document.querySelector('.reset-btn');
 
+window.addEventListener('load', empty);
+
 tipBtns.forEach(item => item.addEventListener('click', (e) => {
     e.preventDefault();
     handleTipClick(item);
@@ -18,14 +20,20 @@ tipInput.addEventListener('input', () => {
     handleTipInput(tipInput);
 });
 
+bill.addEventListener('input', empty);
+
+peopleNumber.addEventListener('input', empty);
+
+resetBtn.addEventListener('click', handleReset);
+
 function handleTipInput(input) {
     const total = calculator(bill.value, input.value, peopleNumber.value).total;
     const tipAmount = calculator(bill.value, input.value, peopleNumber.value).tipAmount;
     console.log(total, tipAmount);
     handleResult(total, tipAmount);
-    tipBtns.forEach((ifChecked) => {
-        ifChecked.classList.remove('selected');
-    });
+    handleError();
+    clearSelection();
+    empty();
 };
 
 function handleTipClick(btn) {
@@ -33,19 +41,49 @@ function handleTipClick(btn) {
     const tipAmount = calculator(bill.value, btn.value, peopleNumber.value).tipAmount;
     console.log(total, tipAmount);
     handleResult(total, tipAmount);
+    handleError();
+    empty();
 };
 
 function handleResult(total, tipAmount) {
     tipAmountText.textContent = `$${tipAmount}`;
     totalAmountText.textContent = `$${total}`;
-}
+};
 
 function handleSelect(btn) {
+    clearSelection();
+    btn.classList.add('selected');
+};
+
+function handleError() {
+    if (peopleNumber.value.length === 0) {
+        errorMsg.textContent = "Can't be zero";
+    } else {
+        errorMsg.textContent = "";
+    }
+};
+
+function handleReset() {
+    bill.value = "";
+    tipInput.value = "";
+    peopleNumber.value = "";
+    clearSelection();
+    empty();
+};
+
+function empty() {
+    if (bill.value == "" && tipInput.value == "" && peopleNumber.value == "") {
+        resetBtn.classList.add('empty')
+    } else {
+        resetBtn.classList.remove('empty')
+    }
+}
+
+function clearSelection() {
     tipBtns.forEach((ifChecked) => {
         ifChecked.classList.remove('selected');
     });
-    btn.classList.add('selected');
-}
+};
 
 function calculator(bill, tip, people) {
     let tipAmount = 0;
